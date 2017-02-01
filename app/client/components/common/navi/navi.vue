a18b3135657cedbef77a992c65f04ab988f4b9f6
kevin <kevin.zhzhang@gmail.com>
2017-02-01 08:41:20 -0500

    navi -home

--- app/client/components/common/navi/navi.vue
+++ app/client/components/common/navi/navi.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="vertical" @select="handleSelect">
+        <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="vertical" @select="handleSelect" :router= route>
             <el-menu-item index="1">处理中心</el-menu-item>
             <el-submenu index="2">
                 <template slot="title">我的工作台</template>
-                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="/home">home</el-menu-item>
                 <el-menu-item index="2-2">选项2</el-menu-item>
                 <el-menu-item index="2-3">选项3</el-menu-item>
             </el-submenu>
@@ -17,6 +17,16 @@
 
 <script>
     export default {
+        data(){
+
+            return{
+                route: true
+
+
+            }
+
+        },
+
         methods: {
             handleSelect(key, keyPath) {
                 console.log(key, keyPath);
